783d50fb8835f5720b28e1c0adbc12a4b558107e
peddadavivek1234 <peddadavivek@gmail.com>
2023-07-29 04:19:34 +0000

    add Api code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/login/Login";
 import Signup from "./components/singup/SingUp";
+import MovieList from "./components/apifetch/ApiFetch";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/movies" element={<MovieList />} />
     </Routes>
   );
 };
--- src/components/apifetch/ApiFetch.js
+++ src/components/apifetch/ApiFetch.js
@@ -1,28 +1,71 @@
-import {Component} from 'react'
-import Cookies from 'js-cookie'
+import React, { useEffect, useState } from "react";
 
-class FetchApi extends Component {
-    state = {
-        moviesData: [],
-    }
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
 
-    componentDidMount() {
-        this.getMoviesData()
-    }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    getMoviesData = async () => {
-        const jwtToken = Cookies.get('jwt_token')
-        const apiUrl = 'https://hoblist.com/api/movieList'
-        const options = {
-            headers: {
-                Authorization: `Bearer ${jwtToken}`
-            },
-            method: 'POST',
+  const fetchMovies = () => {
+    // API endpoint and request body
+    const apiUrl = "https://hoblist.com/api/movieList";
+    const requestBody = {
+      category: "movies",
+      language: "kannada",
+      genre: "all",
+      sort: "voting",
+    };
+
+    // Make a POST request to the API
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.result);
+      })
+      .catch((error) => {
+        console.error("Error fetching data from the API:", error);
+      });
+  };
+
+  const handleVote = (movieId, voteType) => {
+    // Find the movie with the given id in the movies array
+    const updatedMovies = movies.map((movie) => {
+      if (movie._id === movieId) {
+        // Increase or decrease the voting count based on the voteType
+        if (voteType === "up") {
+          return { ...movie, voting: movie.voting + 1 };
+        } else if (voteType === "down") {
+          return { ...movie, voting: movie.voting - 1 };
         }
-        const response = await fetch(apiUrl,options)
-        if (response.ok) {
-            const fetchedData = await response.json()
-            const updatedData = fetchedData.
-        }
-    }
-}
+      }
+      return movie;
+    });
+
+    // Update the state with the updated voting count
+    setMovies(updatedMovies);
+  };
+
+  return (
+    <div>
+      <h2>Movie List</h2>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie._id}>
+            {movie.name} - Votes: {movie.voting}
+            <button onClick={() => handleVote(movie._id, "up")}>Up</button>
+            <button onClick={() => handleVote(movie._id, "down")}>Down</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MovieList;
